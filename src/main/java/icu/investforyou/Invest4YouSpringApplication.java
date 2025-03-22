a47197a32d5cfdbed2f6ddd3c69d99bfd198a10f
ture23 <47562456+ture23@users.noreply.github.com>
2025-03-22 18:22:46 +0100

    init

---
+++ src/main/java/icu/investforyou/Invest4YouSpringApplication.java
@@ -0,0 +1,13 @@
+package icu.investforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Invest4YouSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Invest4YouSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icu/investforyou/Invest4YouSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.investforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Invest4YouSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
